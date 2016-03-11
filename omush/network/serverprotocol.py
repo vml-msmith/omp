f095235362638fcdaffa0ec198ecd5c7d027f1a2
Michael Smith <Michael.Smith@vml.com>
2016-03-11 15:59:39 -0600

    Added OMushServerProtocol websocket protocol

---
+++ omush/network/serverprotocol.py
@@ -0,0 +1,4 @@
+from autobahn.asyncio.websocket import WebSocketServerProtocol
+
+class OMushServerProtocol(WebSocketServerProtocol):
+    pass
--- tests/generic_tests.py
+++ tests/generic_tests.py
@@ -1,14 +1,17 @@
 import unittest
 
-class GenericTests(unittest.TestCase):
+class GenericTest(unittest.TestCase):
+
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_get_command_queue_exists(self):
-        pass
+    def test_protocol(self):
+        """Test that OMushServerProtocol exists"""
+        from omush.network.serverprotocol import OMushServerProtocol
+        protocol = OMushServerProtocol()
 
 if __name__ == '__main__':
     unittest.main()
