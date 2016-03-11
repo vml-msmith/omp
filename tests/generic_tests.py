5ba7c05148c2ef0160216f461f3aa1c9a676ac9d
Michael Smith <Michael.Smith@vml.com>
2016-03-11 15:48:46 -0600

    adding generic tests as somewhere to work on tests before refactor.

---
+++ tests/generic_tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class GenericTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_command_queue_exists(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
