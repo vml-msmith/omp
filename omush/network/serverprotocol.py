5577da29e486bd479f255b7657807b0dd42c8639
Michael Smith <Michael.Smith@vml.com>
2016-03-15 14:59:35 -0500

    Add docs to protocol.

--- omush/network/serverprotocol.py
+++ omush/network/serverprotocol.py
@@ -1,12 +1,27 @@
 from autobahn.asyncio.websocket import WebSocketServerProtocol
 
 class OMushServerProtocol(WebSocketServerProtocol):
+    """WebSocketServerProtocol for use on oMush server.
+
+    A new instance of the protocol is created for each connected client. The
+    protocol will contain a "factory" property by default that points to the
+    server factory. That factory should contain any information needed to link
+    each connection to the overall game.
+    """
+
     def notify(self, msg):
+        """Send a String message to the connected websocket.
+
+        The message will be encoded to utf8 before being transmitted.
+        """
         payload = msg.encode('utf8')
         self.sendMessage(payload, isBinary = False)
 
     def onOpen(self):
-        """The connection to the client has been opened
+        """The connection to the websocket has been opened
+
+        Register the connected socket with the game via the clientmanager and
+        provision a client object for this connection.
         """
         self.clientManager = self.factory.getClientManager()
         self.client = self.clientManager.provisionClient(protocolClient=self)
