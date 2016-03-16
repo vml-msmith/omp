715922c74f185a17eed01ce74ccb0f0b7429b93b
Michael Smith <Michael.Smith@vml.com>
2016-03-16 14:48:13 -0500

    added docs to the client object.

--- omush/network/client.py
+++ omush/network/client.py
@@ -1,11 +1,30 @@
 class OMushConnectedClient(object):
+    """A client object that associates a socket connection and a user objec.t
+
+    Handles the messages incomming from the socket.
+    """
     def __init__(self):
         self.user_object = None
 
     def setLoggedInUser(self, user):
+        """Set the logged in user object."""
         self.user_object = user
 
     def handleMessage(self, message):
+        """Handle a string message "input" to be executed from this client.
+
+        This will attempt to match a command from the client and execute it
+        if possible.
+
+        Note there are three levels of commands:
+          - Socket level: Useable by any connected client, does not reqire a
+            user object to be logged in. These are executed first.
+          - Login Screen level: These commands are useable only when there is
+            not a user object associated with the connection. Not yet logged
+            in.
+          - Connected level: All the commands of the game. These include
+            commands built into the server, exits and soft-coded commands.
+        """
         command = self._matchCommandFromMessage(message)
         if command is not None:
             command.execute(client = self,
