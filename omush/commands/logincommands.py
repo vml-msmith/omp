487734da64c84496afa2f7c6190b4fb78af53f9e
Michael Smith <Michael.Smith@vml.com>
2016-03-18 20:50:35 -0500

    CommandLogin and tests.

---
+++ omush/commands/logincommands.py
@@ -0,0 +1,50 @@
+from .command import Command
+import re
+
+import logging
+
+class CommandLogin(Command):
+    command = "connect"
+    @classmethod
+    def _get_matchers(cls):
+        return [re.compile('^' + cls.command + '\s+[\'|\"](?P<name>[^\"][^\']+)[\'|\"]*\s+(?P<password>\S+)$'),
+                re.compile('^' +  cls.command + '\s+(?P<name>\S+)\s+(?P<password>\S+)$')]
+
+    @classmethod
+    def match(cls, pattern):
+        # Search for this one negative. It'll match:
+        # connect '' password
+        # If we don't do this, the next normal form will count this as correct.
+        # Todo(msmith): Fix the next regex instead.
+        p = re.compile('^' + cls.command + '\s+[\'|\"][\'|\"]*\s+(\S+)$')
+        if p.match(pattern) is not None:
+            return False
+
+        for p in cls._get_matchers():
+            match = p.match(pattern)
+            if match is not None:
+                return True
+
+        return False
+
+    @classmethod
+    def get_args(cls, pattern):
+        for p in cls._get_matchers():
+            match = p.match(pattern)
+            if match is not None:
+                return match.groupdict()
+
+    @classmethod
+    def execute(cls,
+                pattern=None,
+                client=None,
+                obj=None,
+                game=None):
+        args = cls.get_args(pattern)
+        db = game.database
+        player = db.find_player_by_name(args['name'])
+        if player is not None:
+            if player.match_password(args['password']):
+                client.set_logged_in_user(player)
+
+        # Search for name in DB. Oh wait! We don't have a database!
---
+++ tests/commandlogin_tests.py
@@ -0,0 +1,109 @@
+import unittest
+from omush.commands.logincommands import CommandLogin
+
+class MockClient(object):
+    def __init__(self):
+        self.output = None
+        self.is_closed = False
+
+    def close(self):
+        self.is_closed = True
+
+    def notify(self, message):
+        self.output = message
+
+
+class MockClient(object):
+    def __init__(self):
+        self.user_object = None
+
+    def set_logged_in_user(self, user):
+        self.user_object = user;
+
+class MockGame(object):
+    def __init__(self):
+        self.database = MockDatabase()
+
+class MockPlayerObject(object):
+
+    def __init__(self, name, password):
+        self._password = password
+        self.name = name
+
+    def match_password(self, password):
+        if self._password == password:
+            return True
+        return False
+
+class MockDatabase(object):
+    def __init__(self):
+        self.objects = []
+
+    def find_player_by_name(self, name):
+        for k in self.objects:
+            if k.name == name:
+                return k
+
+        return None
+
+class CommandLoginTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_command_login_matches_input(self):
+        """Command should match the pattern:
+
+        connect <username> <password>
+        connect '<spaced> <username>' <password>
+
+        The command should be case insensitive for the connect portion.
+        """
+
+        self.assertTrue(CommandLogin.match("connect michael password"))
+        self.assertTrue(CommandLogin.match("connect    michael password"))
+        # Require exactly two arguments
+        self.assertFalse(CommandLogin.match("login michael smith password"))
+        self.assertFalse(CommandLogin.match("connect michael smith password"))
+        self.assertFalse(CommandLogin.match("connect"))
+        self.assertFalse(CommandLogin.match("connect michael"))
+        self.assertTrue(CommandLogin.match('connect "michael smith" password'))
+        self.assertFalse(CommandLogin.match('connect "" password'))
+
+
+    def test_arg_matcher(self):
+        """Test the arg matcher returns the correct args"""
+        args = CommandLogin.get_args("connect michael password")
+        self.assertEquals(args['name'], 'michael')
+        self.assertEquals(args['password'], 'password')
+        args = CommandLogin.get_args("connect 'michael smith' password")
+        self.assertEquals(args['name'], 'michael smith')
+        self.assertEquals(args['password'], 'password')
+
+    def test_execute(self):
+        client = MockClient()
+        client.user_object = None
+        game = MockGame()
+        game.database.objects.append(MockPlayerObject('michael', 'password'))
+
+        CommandLogin.execute(pattern='connect michael password',
+                             client=client,
+                             obj=None,
+                             game=game)
+        self.assertNotEquals(client.user_object, None)
+        client.user_object = None
+        CommandLogin.execute(pattern='connect michael pass',
+                             client=client,
+                             obj=None,
+                             game=game)
+        self.assertEquals(client.user_object, None)
+        CommandLogin.execute(pattern='connect other password',
+                             client=client,
+                             obj=None,
+                             game=game)
+        self.assertEquals(client.user_object, None)
+
+if __name__ == '__main__':
+    unittest.main()
