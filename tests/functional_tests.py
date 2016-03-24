4c30de11f2e8aaac65712aa984083028c7f065b4
Michael Smith <Michael.Smith@vml.com>
2016-03-24 16:55:01 -0500

    Remove on-purpose failing test.

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -8,9 +8,5 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_does_fail_when_we_want(self):
-        self.fail("Finish")
-        pass
-
 if __name__ == '__main__':
     unittest.main()
